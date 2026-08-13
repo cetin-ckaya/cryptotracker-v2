ab4bf43fb8fb7e672eda82d8f467dde3fa554163
Çetin ÇETİNKAYA <cetincetinctk57@icloud.com>
2026-08-13 18:18:19 +0300

    chore: initial Spring Boot project setup with dependencies

---
+++ backend/cryptotracker-backend/src/test/java/com/cryptotracker/rbackend/CryptotrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptotracker.rbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptotrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
